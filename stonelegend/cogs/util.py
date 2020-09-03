37fb1964d9cad9eebe27eea380c0dbb7ee4ec540
Quanta <abhijeet.nkt@gmail.com>
2020-09-03 17:19:12 +0530

    Giveaways!

--- stonelegend/cogs/util.py
+++ stonelegend/cogs/util.py
@@ -7,6 +7,7 @@ import aioscheduler
 import asyncio
 from typing import Union
 import emoji
+import random
 
 from .. import StoneLegendBot
 from ..db import Database
@@ -18,18 +19,30 @@ class Utility(Cog):
         self.bot = bot
         self._scheduler = aioscheduler.TimedScheduler()
         self.poll_countdown_updater.start()
+        self.giveaway_countdown_updater.start()
         asyncio.ensure_future(self.schedule_from_db())
+
+    async def _schedule_or_call(self, future, when):
+        """Schedules the future to run at given datetime, awaits right away if
+        when > currentdatetime"""
+
+        if datetime.utcnow() > when:
+            await future
+        else:
+            self._scheduler.schedule(future, when)
 
     @loop(seconds=5)
     async def poll_countdown_updater(self):
         """Updates countdown on all poll messages in the db"""
 
         await self.bot.wait_until_ready()
-        now = datetime.utcnow()
 
         for poll_row in await self.bot.db.get_all_polls():
 
-            duration_delta = timedelta(seconds=round(poll_row['finish_time'] - now.timestamp()))
+            if datetime.utcnow().timestamp() > giveaway_row['finish_time']:
+                continue
+
+            duration_delta = timedelta(seconds=round(poll_row['finish_time'] - datetime.utcnow().timestamp()))
 
             try:
                 channel = await self.bot.fetch_channel(poll_row['channel_id'])
@@ -54,6 +67,41 @@ class Utility(Cog):
             except NotFound:
                 pass
 
+    @loop(seconds=5)
+    async def giveaway_countdown_updater(self):
+        """Updates countdown on all giveaway messages in the db"""
+
+        await self.bot.wait_until_ready()
+
+        for giveaway_row in await self.bot.db.get_all_giveaways():
+
+            if datetime.utcnow().timestamp() > giveaway_row['finish_time']:
+                continue
+
+            try:
+                channel = await self.bot.fetch_channel(giveaway_row['channel_id'])
+                message = await channel.fetch_message(giveaway_row['message_id'])
+            except NotFound:
+                # No longer relevent
+                await self.bot.db.delete_giveaway(giveaway_row['id'])
+                continue
+
+            duration_delta = timedelta(seconds=round(giveaway_row['finish_time'] - datetime.utcnow().timestamp()))
+
+            embed = Embed(
+                title="Giveaway!",
+                description=f"{giveaway_row['prize']}\n\n"
+                    + f"*Time left: {duration_delta}*\n"
+                    + f"*Hosted by: <@{giveaway_row['author_id']}>*",
+                color=Color.orange()
+            ).set_footer(text="React with \N{party popper} to enter")
+
+            try:
+                await message.edit(embed=embed)
+            except NotFound:
+                pass
+
+
     @Cog.listener('on_reaction_add')
     async def on_reaction_add(self, reaction: Reaction, user: Union[User, Member]):
         """Listens on reactions to remove multiple poll entries from same user"""
@@ -67,21 +115,16 @@ class Utility(Cog):
                     await other_reaction.remove(user)
 
     async def schedule_from_db(self):
-        self._scheduler.start()
+        self._scheduler.start() # Prepare scheduler
         await self.bot.wait_until_ready()
 
         for poll_row in await self.bot.db.get_all_polls():
-            
-            # Trigger off tasks which finished in past
-            if datetime.utcnow().timestamp() > poll_row['finish_time']:
-                await self.finish_poll(poll_row)
-                continue
+            await self._schedule_or_call(
+                self.finish_poll(poll_row), datetime.fromtimestamp(poll_row['finish_time']))
 
-            # Schedule upcoming tasks
-            self._scheduler.schedule(
-                self.finish_poll(poll_row),
-                datetime.fromtimestamp(poll_row['finish_time'])
-            )
+        for giveaway_row in await self.bot.db.get_all_giveaways():
+            await self._schedule_or_call(
+                self.finish_giveaway(giveaway_row), datetime.fromtimestamp(giveaway_row['finish_time']))
 
     async def finish_poll(self, poll_row):
         """Called when the poll finishes- i.e. when the poll time is up"""
@@ -110,6 +153,32 @@ class Utility(Cog):
         finally:
             await self.bot.db.delete_poll(poll_row['id'])
 
+    async def finish_giveaway(self, giveaway_row):
+        """Called when the giveaway finishes- i.e. when the giveaway time is up"""
+
+        try:
+            channel = await self.bot.fetch_channel(giveaway_row['channel_id'])
+            message = await channel.fetch_message(giveaway_row['message_id'])
+            reaction = utils.get(message.reactions, emoji='\N{party popper}')
+
+            choices = tuple(user.mention for user in await reaction.users().flatten() if user != self.bot.user)
+            if not choices:
+                await channel.send(f"Oh no! Looks like nobody wants {giveaway_row['prize']}")
+                return
+
+            winner = random.choice(choices)
+            await channel.send(f"\N{party popper} {winner} won {giveaway_row['prize']}!")
+
+            await message.edit(embed=Embed(
+                title="Giveaway!",
+                description=f"{giveaway_row['prize']}\n\n"
+                    + f"Winner: {winner}\n"
+                    + f"Hosted by: <@{giveaway_row['author_id']}>"
+            ))
+
+        finally:
+            await self.bot.db.delete_giveaway(giveaway_row['id'])
+            
     @command(name='poll')
     async def poll(self, ctx: Context, duration: str,
         emoji1: Union[Emoji, str], emoji2: Union[Emoji, str], *, question: str):
@@ -164,22 +233,56 @@ class Utility(Cog):
             str(emoji2)
         )
 
-        async def trigger_poll():
-            await self.finish_poll({
-                'id': poll_id,
-                'channel_id': ctx.channel.id,
-                'message_id': message.id,
-                'question': question,
-                'emoji1': emoji1,
-                'emoji2': emoji2
-            })
+        future = self.finish_poll({
+            'id': poll_id,
+            'channel_id': ctx.channel.id,
+            'message_id': message.id,
+            'question': question,
+            'emoji1': emoji1,
+            'emoji2': emoji2
+        })
 
-        try:
-            self._scheduler.schedule(trigger_poll, finish_time)
-        except ValueError:
-            # Cannot shedule, finish time already passed; trigger away
-            await trigger_poll()
+        await self._schedule_or_call(future, finish_time)
 
+    @command(name='giveaway', aliases=('gw',))
+    async def start_giveaway(self, ctx: Context, duration: str, *, prize):
+        """Starts a give away"""
+
+        # Parse and validate duration
+        duration_delta = self.parse_duration(duration)
+        if duration_delta is None:
+            await ctx.send(embed=Embed(
+                description='Invalid duration!',
+                color=Color.orange()
+            ))
+
+            return
+
+        finish_time = datetime.utcnow() + duration_delta
+        
+        embed = Embed(
+            title="Giveaway!",
+            description=f"{prize}\n\n"
+                + f"*Time left: {duration_delta}*\n"
+                + f"*Hosted by: {ctx.author.mention}*",
+            color=Color.orange()
+        ).set_footer(text="React with \N{party popper} to enter")
+
+        message = await ctx.send(embed=embed)
+        await message.add_reaction('\N{party popper}')
+
+        giveaway_id = await self.bot.db.insert_giveaway(ctx.channel.id, message.id,
+            prize, finish_time.timestamp(), ctx.author.id)
+    
+        future = self.finish_giveaway({
+            'id': giveaway_id,
+            'channel_id': ctx.channel.id,
+            'message_id': message.id,
+            'author_id': ctx.author.id,
+            'prize': prize
+        })
+
+        await self._schedule_or_call(future, finish_time)
 
     def parse_duration(self, duration_string: str) -> timedelta:
         pattern = re.compile(r"^((?P<weeks>\d+)w)?((?P<days>\d+)d)?((?P<hours>\d+)h)?((?P<minutes>\d+)m)?((?P<seconds>\d+)s)?$", re.IGNORECASE)
