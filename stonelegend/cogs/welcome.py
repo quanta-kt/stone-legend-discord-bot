7559830bee5989db164d616907e90475ae053f81
Quanta <abhijeet.nkt@gmail.com>
2020-09-06 16:36:57 +0530

    Random backgrounds for welcome cards!

--- stonelegend/cogs/welcome.py
+++ stonelegend/cogs/welcome.py
@@ -23,21 +23,23 @@ class Welcome(Cog):
         self.thread_pool.shutdown()
         self.http_session.close()
 
-    def _generate_welcome_image(self, pfp_data, username):
-        svg = self.template_svg % (b64encode(pfp_data).decode('utf-8'), username)
+    def _generate_welcome_image(self, pfp_data, bg_data, username):
+        svg = self.template_svg % dict(pfp=b64encode(pfp_data).decode('utf-8'),
+            bg=b64encode(bg_data).decode('utf-8'), username=username)
         result = BytesIO()
         cairosvg.svg2png(svg, write_to=result)
         result.seek(0)
         return result
 
-    async def generate_welcome_image(self, pfp_data: bytes, username: str) -> BytesIO:
+    async def generate_welcome_image(self, pfp_data: bytes, bg_data: bytes, username: str) -> BytesIO:
         """Build and return the png image from svg template as BytesIO object"""
 
         return await self.bot.loop.run_in_executor(self.thread_pool, self._generate_welcome_image,
-            pfp_data, username)
+            pfp_data, bg_data, username)
 
-    @Cog.listener()
-    async def on_member_join(self, member: Member):
+    #@Cog.listener()
+    @command('_welc')
+    async def on_member_join(self, ctx: Context, member: Member):
         """Listens to member join event to welcome them"""
 
         target_channel_id = await self.bot.db.get_welcome_channel(member.guild.id)
@@ -51,7 +53,10 @@ class Welcome(Cog):
         async with self.http_session.get(str(member.avatar_url_as(format='png'))) as resp:
             pfp = await resp.content.read()
 
-        image = await self.generate_welcome_image(pfp, str(member))
+        async with self.http_session.get("https://source.unsplash.com/500x250/?universe") as resp:
+            bg = await resp.content.read()
+
+        image = await self.generate_welcome_image(pfp, bg, str(member))
         del pfp
 
         await target_channel.send(member.mention, file=File(image, filename='welcome.png'))
